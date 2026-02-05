a3499dfd3986ecb7dd95ad2ef94346de0d606b90
NargizSafiyarli <nergizsefiyarli@gmail.com>
2026-02-06 00:49:13 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args){
+        int i=5;
+        do
+        {
+            i++;
+            System.out.println(i);
+        }while(i>1 && i<10);
+    }
+}
