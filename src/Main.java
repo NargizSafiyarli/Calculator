08cc4bd94d302cb17741b5e5acaa5afc9d8603bf
NargizSafiyarli <nergizsefiyarli@gmail.com>
2026-02-06 11:51:38 +0400

    changing variables

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 class Main{
     public static void main(String[] args){
-        int num1=  Integer.parseInt(args[0]);
+        int number1=  Integer.parseInt(args[0]);
         String op= args[1];
-        int num2=  Integer.parseInt(args[2]);
+        int number2 =  Integer.parseInt(args[2]);
         int result=0;
         switch(op){
             case "+" -> {
-                result = num1+num2;
+                result = number1+ number2;
             }
-            case "-" -> {result = num1-num2;
+            case "-" -> {result = number1- number2;
             }
-            case "/" -> {result = num1/num2;
+            case "/" -> {result = number1/ number2;
             }
-            case "*" -> { result = num1*num2;
+            case "*" -> { result = number1* number2;
             }
         }
         System.out.println(result);
